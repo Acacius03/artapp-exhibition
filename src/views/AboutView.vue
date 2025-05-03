06fd100b4c993a605375118ed1e7d0b6dcee6f48
Acacius <111176282+Acacius03@users.noreply.github.com>
2025-05-03 23:44:40 +0800

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,58 +3,65 @@ import { RouterLink } from 'vue-router'
 import IconArrowRight from '../components/icons/IconArrowRight.vue'
 </script>
 <template>
-  <main class="screen">
-    <section>
-      <header>
-        <h1>Overview</h1>
-      </header>
-      <p>
-        <i>
-          <strong>"Pearl of the Orient: Filipino by Nature"</strong>
-        </i>
-        is our love letter to the soul of the Philippines — vibrant, proud, and unstoppable. This
-        isn't just an exhibit. It's a celebration. A heartbeat. A mirror held up to who we are —
-        shaped by islands, woven by stories, and powered by a spirit that never backs down.
-      </p>
-      <p>
-        Being Filipino isn't just in our blood — it's in our laugh, our grit, our fiesta spirit.
-        It's in the way we dance through storms, find beauty in struggle, and keep our culture alive
-        through every song, story, and shared meal. From bold traditions to everyday triumphs,
-        Filipino by Nature brings together the threads that make us who we are — fierce, joyful,
-        creative, and endlessly proud.
-      </p>
-      <p>
-        Step into our story. Feel the rhythm. Celebrate your roots. Because being Filipino? It's not
-        just what we are. It's how we live.
-      </p>
-    </section>
-    <RouterLink to="home" class="fab">
-      <IconArrowRight />
-    </RouterLink>
-    <footer id="footer">
-      <p>
-        Image Use Disclaimer: The images of artworks displayed on this website are used for
-        informational, educational, and non-commercial purposes only, as part of a virtual
-        exhibition. All rights to the artworks remain with the original artists, copyright holders,
-        or contributing institutions.
-      </p>
-      <p>
-        Virtual Exhibit created by: <strong>Krishelle Ann Barbin</strong>,
-        <strong>Felix Erickson De Loyola</strong>, <strong>Rainisia Macabangkit</strong>,
-        <strong>Diana Rose Maraña</strong> and <strong>Normay Pangan</strong>
-      </p>
-    </footer>
-  </main>
+  <div class="screen">
+    <main>
+      <section>
+        <header>
+          <h1>Overview</h1>
+        </header>
+        <p>
+          <i>
+            <strong>"Pearl of the Orient: Filipino by Nature"</strong>
+          </i>
+          is our love letter to the soul of the Philippines — vibrant, proud, and unstoppable. This
+          isn't just an exhibit. It's a celebration. A heartbeat. A mirror held up to who we are —
+          shaped by islands, woven by stories, and powered by a spirit that never backs down.
+        </p>
+        <p>
+          Being Filipino isn't just in our blood — it's in our laugh, our grit, our fiesta spirit.
+          It's in the way we dance through storms, find beauty in struggle, and keep our culture
+          alive through every song, story, and shared meal. From bold traditions to everyday
+          triumphs, Filipino by Nature brings together the threads that make us who we are — fierce,
+          joyful, creative, and endlessly proud.
+        </p>
+        <p>
+          Step into our story. Feel the rhythm. Celebrate your roots. Because being Filipino? It's
+          not just what we are. It's how we live.
+        </p>
+      </section>
+      <RouterLink to="home" class="fab">
+        <IconArrowRight />
+      </RouterLink>
+      <footer>
+        <p>
+          Image Use Disclaimer: The images of artworks displayed on this website are used for
+          informational, educational, and non-commercial purposes only, as part of a virtual
+          exhibition. All rights to the artworks remain with the original artists, copyright
+          holders, or contributing institutions.
+        </p>
+        <p>
+          Virtual Exhibit created by: <strong>Krishelle Ann Barbin</strong>,
+          <strong>Felix Erickson De Loyola</strong>, <strong>Rainisia Macabangkit</strong>,
+          <strong>Diana Rose Maraña</strong> and <strong>Normay Pangan</strong>
+        </p>
+      </footer>
+    </main>
+  </div>
 </template>
 
 <style scoped>
-.screen {
+.screen > main {
+  display: flex;
+  flex-direction: column;
   gap: 1rem;
+  max-width: 768px;
+  margin-inline: auto;
+  flex-grow: 1;
 }
-#footer {
+footer {
   border-top: 1px solid #ccc;
 }
-#footer p {
+footer p {
   text-align: center;
   font-size: 0.9rem;
   color: #777;
@@ -62,7 +69,8 @@ import IconArrowRight from '../components/icons/IconArrowRight.vue'
 }
 h1 {
   font-family: 'Love Light', cursive;
-  font-size: 4rem;
+  font-size: 5rem;
+  text-align: center;
 }
 
 section p {
